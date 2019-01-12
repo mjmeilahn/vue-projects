bb2925acec0a95186a4c3c34a8c5c563b70a006c
mjmeilahn <mjmeilahn@gmail.com>
2019-01-12 15:10:48 -0800

    Adding Advanced Component project

---
+++ 6-advanced-communication-in-components/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="seletedComponent = 'app-quote'">Quote</button>
+                <button @click="seletedComponent = 'app-author'">Author</button>
+                <button @click="seletedComponent = 'app-new'">New</button>
+
+                <hr>
+
+                <p>{{ seletedComponent }}</p>
+
+                <keep-alive>
+                    <component :is="seletedComponent">
+                        <p>Default content</p>
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+
+    export default {
+        data() {
+            return {
+                quoteTitle: 'The Quote',
+                seletedComponent: 'app-quote'
+            }
+        },
+        components: {
+            'app-quote': Quote,
+            'app-author': Author,
+            'app-new': New
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ 6-advanced-communication-in-components/src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+            <span style="color: #ccc;"><slot name="subtitle">The subtitle</slot></span>
+        </div>
+        <hr>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        margin: 30px auto;
+        padding: 30px;
+        text-align: center;
+    }
+
+    .title {
+        font-style: italic;
+    }
+</style>
+
+
